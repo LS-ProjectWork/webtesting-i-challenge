a1689368e4303dd23824fdb6ef25349b8fdb306a
Darien Payton <darien.payton71@gmail.com>
2019-05-13 15:59:02 -0400

    decreases durability by 5 when enhancement is greater is less than 15

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -6,4 +6,9 @@ describe('enhancer.js', () => {
             expect(enhancer.succeed({enhancement: 18})).toEqual({enhancement: 19})
         })
     })
+    describe('fail method', () => {
+        it('decreases durability by 5 when enhancement is less than 15', () => {
+            expect(enhancer.fail({enhancement: 9, durability: 85})).toEqual({enhancement: 9, durability: 80})
+        })
+    })
 })
