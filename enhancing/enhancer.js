1d55014bc4e61761dd5b131a54b18a41edfa5653
Darien Payton <darien.payton71@gmail.com>
2019-05-13 16:01:23 -0400

    decreases durbility by 10 when enhancement is at least 15

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -15,6 +15,8 @@ function succeed(item) {
 function fail(item) {
   if(item.enhancement < 15) {
     item.durability -= 5
+  } else if(item.enhancement >= 15){
+    item.durability -= 10
   }
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -10,5 +10,8 @@ describe('enhancer.js', () => {
         it('decreases durability by 5 when enhancement is less than 15', () => {
             expect(enhancer.fail({enhancement: 9, durability: 85})).toEqual({enhancement: 9, durability: 80})
         })
+        it('decreases durability by 10 when enhancement is at least 15', () => {
+            expect(enhancer.fail({enhancement: 15, durability: 40})).toEqual({enhancement: 15, durability: 30})
+        })
     })
 })
